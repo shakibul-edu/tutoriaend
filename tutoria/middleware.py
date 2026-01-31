51de1e8fbfdfe1a5212c8c46f6ad87560e04e89e
shakibul-edu <shakibulislamj@gmail.com>
2026-02-01 01:08:08 +0600

    Add API cache disabling middleware and refactor settings
    Introduced DisableAPICacheMiddleware to prevent caching of API responses and added it to the middleware stack. Refactored settings.py for improved organization, updated security and CORS settings, and made minor improvements to REST framework, storage, and template configurations.

---
+++ tutoria/middleware.py
@@ -0,0 +1,11 @@
+class DisableAPICacheMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if request.path.startswith("/api/"):
+            response["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+            response["Pragma"] = "no-cache"
+            response["Expires"] = "0"
+        return response
--- tutoria/settings.py
+++ tutoria/settings.py
@@ -4,132 +4,156 @@ import os
 import dj_database_url
 from datetime import timedelta
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+# --------------------------------------------------
+# BASE
+# --------------------------------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 load_dotenv()
 
-# --- SECURITY ---
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-)xtl6)!05!_i&j^xw%8j&c&71_#a@a$$5vs9-kk5e(r^i6d6+_')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# --------------------------------------------------
+# SECURITY
+# --------------------------------------------------
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-)xtl6)!05!_i&j^xw%8j&c&71_#a@a$$5vs9-kk5e(r^i6d6+_",
+)
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS',
-    '.herokuapp.com,tutoriaend.shakibul.me,www.tutoriaend.shakibul.me,localhost,127.0.0.1,etuition.app,www.etuition.app,tuitions.shakibul.me,www.tuitions.shakibul.me, api.etuition.app,www.api.etuition.app'
-).replace(' ', '').split(',')
+    "ALLOWED_HOSTS",
+    ".herokuapp.com,localhost,127.0.0.1,etuition.app,www.etuition.app,api.etuition.app,www.api.etuition.app",
+).replace(" ", "").split(",")
 
 CSRF_TRUSTED_ORIGINS = os.environ.get(
-    'CSRF_TRUSTED_ORIGINS',
-    'https://*.herokuapp.com,https://tutoriaend.shakibul.me,http://localhost,http://127.0.0.1,https://etuition.app,https://api.etuition.app,https://www.etuition.app,https://www.tuitions.shakibul.me'
-).replace(' ', '').split(',')
+    "CSRF_TRUSTED_ORIGINS",
+    "http://localhost,http://127.0.0.1,https://etuition.app,https://www.etuition.app,https://api.etuition.app",
+).replace(" ", "").split(",")
 
 if not DEBUG:
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT', 'True') == 'True'
-    SESSION_COOKIE_SECURE = os.environ.get('SESSION_COOKIE_SECURE', 'True') == 'True'
-    CSRF_COOKIE_SECURE = os.environ.get('CSRF_COOKIE_SECURE', 'True') == 'True'
-    SECURE_HSTS_SECONDS = int(os.environ.get('SECURE_HSTS_SECONDS', 31536000))
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = os.environ.get('SECURE_HSTS_INCLUDE_SUBDOMAINS', 'True') == 'True'
-    SECURE_HSTS_PRELOAD = os.environ.get('SECURE_HSTS_PRELOAD', 'True') == 'True'
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 
-# --- APPLICATION DEFINITION ---
+# --------------------------------------------------
+# APPLICATIONS
+# --------------------------------------------------
 INSTALLED_APPS = [
-    'cloudinary_storage',  # MANDATORY: Must be at the very top
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'cloudinary',
-    'django.contrib.gis',
-    'rest_framework',
-    'corsheaders',
-    'rest_framework_simplejwt',
+    "cloudinary_storage",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.gis",
+    "cloudinary",
+    "rest_framework",
+    "corsheaders",
+    "rest_framework_simplejwt",
     "drf_spectacular",
     "drf_spectacular_sidecar",
-    'base',
+    "base",
 ]
 
+# --------------------------------------------------
+# MIDDLEWARE
+# --------------------------------------------------
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+
+    # Disable cache for API responses (prevents 304)
+    "tutoria.middleware.DisableAPICacheMiddleware",
+
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.contrib.sessions.middleware.SessionMiddleware",
+
+    # CORS must be before CommonMiddleware
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'tutoria.urls'
-AUTH_USER_MODEL = 'base.CustomUser'
+ROOT_URLCONF = "tutoria.urls"
+WSGI_APPLICATION = "tutoria.wsgi.application"
+AUTH_USER_MODEL = "base.CustomUser"
 
-# --- REST FRAMEWORK ---
+# --------------------------------------------------
+# REST FRAMEWORK
+# --------------------------------------------------
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly",
     ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '4/minute',
-        'user': '1000/day'
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "4/minute",
+        "user": "1000/day",
     },
-    'DEFAULT_PARSER_CLASSES': [
-        'rest_framework.parsers.JSONParser',
-        'rest_framework.parsers.FormParser',
-        'rest_framework.parsers.MultiPartParser',
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parsers.JSONParser",
+        "rest_framework.parsers.FormParser",
+        "rest_framework.parsers.MultiPartParser",
+    ],
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRenderer",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
+# --------------------------------------------------
+# CORS
+# --------------------------------------------------
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000", 
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://tutoriaend.shakibul.me",
-    "https://www.tutoriaend.shakibul.me",
-    "https://tuitions.shakibul.me",
-    "https://www.tuitions.shakibul.me",
-    "https://etuition.app",
-    "https://api.etuition.app",
     "https://www.etuition.app",
 ]
+
 CORS_ALLOW_CREDENTIALS = True
 
-# --- DATABASE ---
-if os.environ.get('DATABASE_URL'):
+# --------------------------------------------------
+# DATABASE
+# --------------------------------------------------
+if os.environ.get("DATABASE_URL"):
     DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600, 
+        "default": dj_database_url.config(
+            conn_max_age=600,
             ssl_require=True,
-            engine='django.contrib.gis.db.backends.postgis'
+            engine="django.contrib.gis.db.backends.postgis",
         )
     }
 else:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.contrib.gis.db.backends.postgis',
-            'NAME': 'mygeo',
-            'USER': 'postgres',
-            'PASSWORD': 'sa714010',
-            'HOST': 'db',
-            'PORT': '5432',
+        "default": {
+            "ENGINE": "django.contrib.gis.db.backends.postgis",
+            "NAME": "mygeo",
+            "USER": "postgres",
+            "PASSWORD": "sa714010",
+            "HOST": "db",
+            "PORT": "5432",
         }
     }
 
-# --- STORAGE CONFIGURATION ---
-
-# Cloudinary Credentials
+# --------------------------------------------------
+# STORAGE (CLOUDINARY)
+# --------------------------------------------------
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
 }
 
-# Use the modern STORAGES setting (Django 4.2+)
 STORAGES = {
     "default": {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
@@ -138,39 +162,47 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.StaticCloudinaryStorage",
     },
 }
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticCloudinaryStorage'
-
-STATIC_URL = 'static/'
+STATICFILES_STORAGE = "cloudinary_storage.storage.StaticCloudinaryStorage"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
-# --- REMAINING SETTINGS ---
-WSGI_APPLICATION = 'tutoria.wsgi.application'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-
+# --------------------------------------------------
+# JWT
+# --------------------------------------------------
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ALGORITHM": "HS256",
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
+# --------------------------------------------------
+# TEMPLATES
+# --------------------------------------------------
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
+
+# --------------------------------------------------
+# INTERNATIONALIZATION
+# --------------------------------------------------
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
+USE_I18N = True
+USE_TZ = True
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
